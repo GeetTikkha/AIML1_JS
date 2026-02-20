390a480eed0effe14536b805217527f1fa5ab8e9
GeetTikkha <tikkhageet@gmail.com>
2026-02-20 10:35:36 +0530

    java

--- script.js
+++ script.js
@@ -1,1 +1,53 @@
-console.log("Hello World")
+// console.log("Hello World");
+// document.write("Hello World");
+
+// var num = 10;
+// console.log(num);
+// console.log(typeof(num));
+// num = false;
+// console.log(num);
+// console.log(typeof(num));
+// num = "Geet Tikkha";
+// console.log(num);
+// console.log(typeof(num));
+// num = null;
+// console.log(num);
+// console.log(typeof(num));
+// num = undefined;
+// console.log(typeof(num));
+
+// // function
+// var sayHello = function() {
+//     alert("hello world");
+// }
+
+// // array
+// var numberArray = [1, 2, 3];
+// var animals = new Array("cat", "dog", "mouse", "lion");
+
+// // object
+// var person = 
+
+
+
+
+// const sym1 = Symbol(4);
+// console.log(sym1);
+// const sym2 = Symbol(4);
+// console.log(sym2);
+// if (sym1 == sym2) {
+//     console.log("true");
+// }
+// else
+// {
+//     console.log("false")  // because each symbol is unique
+// }
+
+
+
+var a = 100; var b = 13; var c = 10; var linebreak = "<br>";
+
+document.write("a + b + c =");
+result = a + b + c;
+document.write(result);
+document.write(linebreak);
